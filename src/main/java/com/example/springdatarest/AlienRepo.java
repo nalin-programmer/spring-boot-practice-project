3f476588f0c70a256eb8d5a3ad974bf597f7751b
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2023-01-30 19:20:20 +0530

    repository rest resource added to aliens model

--- src/main/java/com/example/springdatarest/AlienRepo.java
+++ src/main/java/com/example/springdatarest/AlienRepo.java
@@ -2,7 +2,9 @@ package com.example.springdatarest;
 
 import com.example.springdatarest.model.Alien;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "aliens", path = "aliens")
 public interface AlienRepo extends JpaRepository<Alien, Integer> {
 
 }
